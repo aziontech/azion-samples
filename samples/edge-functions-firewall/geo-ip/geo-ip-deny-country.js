69a767517123a0395e8fdfecec30d588f267e482
gabriel-azion <gabriel.franca@azion.com>
2023-02-24 11:36:05 -0300

    feat: add geoip and add header sample codes

---
+++ samples/edge-functions-firewall/geo-ip/geo-ip-deny-country.js
@@ -0,0 +1,16 @@
+async function firewallHandler(event){
+    // Access the country code through geoip
+    let countryCode = event.request.metadata["geoip_country_code"]
+
+    // Do some logic here
+    // In this example, we are blocking access from Brazil
+    if (countryCode ==="BR"){
+        event.deny();
+    }
+
+    // Then, if it comes from any other country,
+    // the processing continues
+    event.continue();
+}
+
+addEventListener("firewall", (event)=>event.waitUntil(firewallHandler(event)));
