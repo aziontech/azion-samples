23043e6e65a4f6a07f9f543fca4ee1c36e72d702
Jose Filho <jc.bsfilho@gmail.com>
2023-09-21 12:50:40 -0300

    fix: change handler main

--- templates/upstash-geolocation/main.js
+++ templates/upstash-geolocation/main.js
@@ -1,11 +1,5 @@
 import { handleRequest } from "./src/function/index.js";
 
-return await handleRequest(event?.request, event?.args);
-
-// async function handleEvent(event) {
-//   __HANDLER__;
-// }
-
-// addEventListener("fetch", (event) => {
-//   event.respondWith(handleEvent(event));
-// });
+async function main(event){
+    return handleRequest(event?.request, event?.args);
+}
