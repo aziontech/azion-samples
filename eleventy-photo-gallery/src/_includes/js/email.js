c7c2fea3b748f27ac0ec23ebabbe22625cbe4961
Pablo Diehl <pablodiehl@ymail.com>
2024-04-03 13:48:33 -0300

    fix(eleventy): remove misplaced folder

--- eleventy-photo-gallery/src/_includes/js/email.js
+++
@@ -1,9 +0,0 @@
-// update this variable with your email :)
-const email = "foobar@gmail.com";
-const submit = document.querySelector(".reach-me");
-
-function sendMail() {
-    const subject = document.querySelector(".email-subject").value;
-    const message = document.querySelector(".email-msg").value;
-    window.location.href = `mailto:${email}?subject=${subject}&body=${message}`;
-}
