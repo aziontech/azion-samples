efbce67b95514503212a1c851e4c3936800dcffc
Pablo Diehl <pablodiehl@ymail.com>
2024-04-03 13:08:42 -0300

    feat(eleventy): eleventyst-starter-kit

---
+++ templates/eleventyst-starter-kit/.eleventy.js
@@ -0,0 +1,59 @@
+const EleventyVitePlugin = require('@11ty/eleventy-plugin-vite');
+const EleventyWebcPlugin = require('@11ty/eleventy-plugin-webc');
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(EleventyVitePlugin);
+  eleventyConfig.addPlugin(EleventyWebcPlugin, {
+    components: 'src/_includes/components/**/*.webc',
+  });
+
+  // Static assets to pass through
+  eleventyConfig.addPassthroughCopy('./src/images');
+  eleventyConfig.addPassthroughCopy('./src/public');
+  eleventyConfig.addPassthroughCopy('./src/styles');
+  eleventyConfig.addPassthroughCopy('./src/main.js');
+
+  eleventyConfig.setServerOptions({
+    // Default values are shown:
+
+    // Whether the live reload snippet is used
+    liveReload: true,
+
+    // Whether DOM diffing updates are applied where possible instead of page reloads
+    domDiff: true,
+
+    // The starting port number
+    // Will increment up to (configurable) 10 times if a port is already in use.
+    port: 8080,
+
+    // Additional files to watch that will trigger server updates
+    // Accepts an Array of file paths or globs (passed to `chokidar.watch`).
+    // Works great with a separate bundler writing files to your output folder.
+    // e.g. `watch: ["_site/**/*.css"]`
+    watch: [],
+
+    // Show local network IP addresses for device testing
+    showAllHosts: false,
+
+    // Use a local key/certificate to opt-in to local HTTP/2 with https
+    https: {
+      // key: "./localhost.key",
+      // cert: "./localhost.cert",
+    },
+
+    // Change the default file encoding for reading/serving files
+    encoding: 'utf-8',
+  });
+
+  return {
+    dir: {
+      input: 'src',
+      output: '_site',
+    },
+    passthroughFileCopy: true,
+    templateFormats: ['html', 'md', 'webc'],
+    htmlTemplateEngine: 'webc',
+    dataTemplateEngine: 'webc',
+    markdownTemplateEngine: 'webc',
+  };
+};
