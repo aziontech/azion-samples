49c468af967f9cf4c3f2a4a321a971fab72a673c
Pablo Diehl <pablodiehl@ymail.com>
2024-05-03 13:44:09 -0300

    fix(upstash-ratelimit): function export

--- templates/upstash-ratelimit/main.js
+++ templates/upstash-ratelimit/main.js
@@ -3,3 +3,5 @@ import { handleRequest } from "./src/function/index.js";
 async function main(event){
     return handleRequest(event?.request, event?.args);
 }
+
+export default main;                         
