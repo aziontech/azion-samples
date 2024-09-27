6532d905fc8752431c8f716530fef1bf4db81258
José Filho <jc.bsfilho@gmail.com>
2024-09-27 11:51:25 -0300

    fix: update azion.config.mjs in JS templates

---
+++ templates/javascript/drizzle-neondb-sample/azion.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from "azion";
+
+export default defineConfig({
+  build: {
+    entry: "main.js",
+    preset: {
+      name: "javascript",
+    },
+    polyfills: true,
+  },
+});
