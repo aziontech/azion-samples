42b6543269a2e95a678882801d6e097967fc6845
Pablo Diehl <pablodiehl@ymail.com>
2024-10-10 14:12:37 -0300

    chore(templates): Remove challenge-braziljs-2024
    This template shouldn't be used anymore.

--- templates/astro/challenge-braziljs-2024/src/components/countdown/index.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <Countdown mainColor="#B5B5B5" secondFlipColor="#EDEDED" mainFlipBackgroundColor="#222222" secondFlipBackgroundColor="#353535" labelColor="#B5B5B5" deadline="2024-05-09 09:00:00" countdownSize="6rem" />
-</template>
-
-<script setup>
-import { Countdown } from "vue3-flip-countdown";
-
-</script>
