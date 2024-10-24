6dc9ba72dc46afb207c0b5102dc96423e55a3572
Pablo Diehl <pablodiehl@ymail.com>
2024-10-24 10:17:26 -0300

    feat(templates/nuxt-sanity): Add template

---
+++ templates/nuxt/sanity-nuxt-clean/components/BlockContent.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  props: {
+    blocks: {
+      type: Array,
+      required: true,
+    },
+  },
+}
+</script>
+
+<template>
+  <SanityContent :blocks="blocks" />
+</template>
