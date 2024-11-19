0da5515642411d3c57205f626f77ff6238ae031d
Pablo Diehl <pablodiehl@ymail.com>
2024-11-19 09:37:17 -0300

    feat(templates/vuepress): Vuepress hope blog

---
+++ templates/vuepress/vuepress-hope-blog/docs/.vuepress/config.ts
@@ -0,0 +1,38 @@
+import { viteBundler } from "@vuepress/bundler-vite";
+import { defineUserConfig } from "vuepress";
+import { getDirname, path } from "vuepress/utils";
+
+import theme from "./theme.js";
+
+const __dirname = getDirname(import.meta.url);
+
+export default defineUserConfig({
+  base: "/",
+
+  locales: {
+    "/": {
+      lang: "en-US",
+      title: "Blog Demo",
+      description: "A blog demo for vuepress-theme-hope",
+    },
+    "/zh/": {
+      lang: "zh-CN",
+      title: "博客演示",
+      description: "vuepress-theme-hope 的博客演示",
+    },
+  },
+
+  bundler: viteBundler(),
+
+  theme,
+
+  // enable it with pwa
+  // shouldPrefetch: false,
+
+  alias: {
+    "@theme-hope/modules/blog/components/BlogHero": path.resolve(
+      __dirname,
+      "./components/BlogHero.vue",
+    ),
+  },
+});
