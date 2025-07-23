f2006264d05f3ac397525f0216e4f2da95600e92
Bruno Germano <bruno.germano@gmail.com>
2025-07-23 12:42:13 -0300

    chore: remove test files and config files across samples

--- samples/file-upload/eslint.config.js
+++
@@ -1,6 +0,0 @@
-import tseslint from 'typescript-eslint';
-
-export default tseslint.config({
-  files: ["**/*.ts"],
-  extends: [tseslint.configs.recommended],
-});
--- samples/resend/azion.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'azion';
-
-export default defineConfig({
-  build: {
-    entry: 'src/index.ts',
-    worker: true,
-    preset: 'typescript',
-  },
-});
