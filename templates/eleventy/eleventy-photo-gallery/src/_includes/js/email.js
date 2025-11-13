60c7d7ce1f5e8bbcd6dddadaa07eb01cc8b89ec0
pablodiehl <pablodiehl@ymail.com>
2025-11-13 14:23:25 -0300

    fix(templates/eleventy-photo-gallery): code scan issues

--- templates/eleventy/eleventy-photo-gallery/src/_includes/js/email.js
+++ templates/eleventy/eleventy-photo-gallery/src/_includes/js/email.js
@@ -5,5 +5,5 @@ const submit = document.querySelector(".reach-me");
 function sendMail() {
     const subject = document.querySelector(".email-subject").value;
     const message = document.querySelector(".email-msg").value;
-    window.location.href = `mailto:${email}?subject=${subject}&body=${message}`;
+    window.location.href = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(message)}`;
 }
