9e021f92a53e97f92a02b8d42ce569837833d91b
pablodiehl <pablodiehl@ymail.com>
2025-12-08 10:36:02 -0300

    feat(v3): postgres-sveltekit

---
+++ templates/svelte/postgres-sveltkit/svelte.config.js
@@ -0,0 +1,12 @@
+import adapter from 'azion/preset/sveltekit';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  kit: {
+    adapter: adapter(),
+  },
+  preprocess: vitePreprocess(),
+}
+
+export default config
