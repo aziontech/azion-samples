83b1f44adf71fb628d001f7442ee3ae443497441
Pablo Diehl <pablodiehl@ymail.com>
2025-12-08 14:21:12 -0300

    feat(templates): paint by text (#343)
    * feat(templates/opennextjs): paint by text
    
    * chore: update dependencies paint-by-text

---
+++ templates/opennextjs/paint-by-text/pages/api/predictions/index.js
@@ -0,0 +1,38 @@
+import Replicate from "replicate";
+import packageData from "../../../package.json";
+
+const API_HOST = process.env.REPLICATE_API_HOST || "https://api.replicate.com";
+
+export default async function handler(req, res) {
+  const token = req.headers["x-replicate-api-token"];
+  if (!token) {
+    res.statusCode = 401;
+    res.end(JSON.stringify({ detail: "Missing Replicate API token. Please provide your token in the x-replicate-api-token header." }));
+    return;
+  }
+  // remove null and undefined values
+  req.body = Object.entries(req.body).reduce(
+    (a, [k, v]) => (v == null ? a : ((a[k] = v), a)),
+    {}
+  );
+  let prediction;
+  const model = "black-forest-labs/flux-kontext-pro";
+  const replicate = new Replicate({
+    auth: token,
+    userAgent: `${packageData.name}/${packageData.version}`
+  });
+  prediction = await replicate.predictions.create({
+    model,
+    input: req.body
+  });
+  res.statusCode = 201;
+  res.end(JSON.stringify(prediction));
+}
+
+export const config = {
+  api: {
+    bodyParser: {
+      sizeLimit: "10mb",
+    },
+  },
+};
