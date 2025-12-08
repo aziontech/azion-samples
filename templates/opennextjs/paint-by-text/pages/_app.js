cbdad3865517afe42e7b00ca67fe9ece82411dd1
pablodiehl <pablodiehl@ymail.com>
2025-12-08 09:24:44 -0300

    chore: update dependencies paint-by-text

--- templates/opennextjs/paint-by-text/pages/_app.js
+++ templates/opennextjs/paint-by-text/pages/_app.js
@@ -1,11 +1,9 @@
 import "../styles/globals.css";
-import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
-      <Analytics />
     </>
   );
 }
