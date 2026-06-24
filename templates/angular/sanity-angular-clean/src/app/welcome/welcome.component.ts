af711437e754947685e3251a9ce7e9655a97439a
pablodiehl <pablodiehl@ymail.com>
2026-06-24 08:58:08 -0300

    chore: upgrade angular version for angular-sanity

--- templates/angular/sanity-angular-clean/src/app/welcome/welcome.component.ts
+++ templates/angular/sanity-angular-clean/src/app/welcome/welcome.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-welcome',
+  standalone: false,
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.css']
 })
